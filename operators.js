ba28a22882688b2df28dbd81a35e4bc37ffaffbb
Local Man <mirriamthigari@gmail.com>
2025-01-26 23:09:01 +0300

    operators in js

---
+++ operators.js
@@ -0,0 +1,17 @@
+//used to perfom different types of logical and mathematical computations
+// arithmetic operators.perfoms arithmetics operations.
+ let a = 5;
+ let b = 7;
+  function getSum(){
+    return  a+b;
+  }
+   function getExponential(){
+    return a**b;
+   }
+
+   
+   console.log("sum:", getSum());
+   console.log("exponential:", getExponential());
+   // assignment operators assigns values to variables
+    var i = 10;
+     
