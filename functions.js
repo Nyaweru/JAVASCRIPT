154e00e30f7681c500c65a8a42fcfa03279c2f62
Local Man <mirriamthigari@gmail.com>
2025-01-28 23:22:38 +0300

    learn about functions

---
+++ functions.js
@@ -0,0 +1,32 @@
+// block of code that performs a task and is executed when invoked
+/* syntax : Function keyword then name , parameters that are in brackets () ie function Product(10.9);
+code to be execruted is inside the curly brackets {}
+arguments are the values received when function is invoked
+function is invoked if an event occurs ie, click event, automatically invoked, invoked in javascript code
+the operator () calls the function
+*/
+
+function getDifference(){
+    let x = 5;
+    let y = 3;
+    return x-y ;
+ 
+}
+console.log("difference:", getDifference());
+// also 
+ let  a = getDifference(5,3);// 5 and 3 are parameters
+  function getDifference(i,j){
+    return a-b ;
+  }
+  
+
+  // functions as variable values
+  
+let text = "Happy coding with  " + getLanguage() + " .";
+document.getElementById("demo").innerHTML = text;
+
+function getLanguage() {
+  return ("JAVASCRIPT")
+
+  }
+  //local variables are only the ones inside a function.
