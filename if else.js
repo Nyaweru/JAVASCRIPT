286b6afaef30544aa963a2c30b18fd2908808774
Local Man <mirriamthigari@gmail.com>
2025-02-14 19:13:26 +0300

    checks the name of the student assigned to a number

---
+++ if else.js
@@ -0,0 +1,8 @@
+//use if to specify a block of code to be executed if the condition is true
+// use else to specify block of code to be executed if the same condition is false
+// use else if to specify a new condition to test id the first statement is false
+//use switch to specify many alternative blocks of code
+let num = 200;
+if (num < 200) {
+    console.log("num is less");
+}
